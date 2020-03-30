0313ae6071c01703a5a7a19b3875517d1a657b71
Adao Natalino <sansonov@gmail.com>
2020-03-30 21:42:13 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,14 @@
 def find_element_index(array, value_to_find)
   i = 0
+  a = []
   while i<array.length do
-  if array.[i] === value_to_find
+    if array.[i] === value_to_find
     return i
+    a.push(value_to_find)
+    end
     i+=1
+  end
+  if a.length === array.length
+    return nil
   end
 end
